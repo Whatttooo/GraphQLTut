5e4839e95fd0ce2d1c9184afdb5e9838a96c4e4d
Josh Whateley <j.whateley@hotmail.co.nz>
2017-11-06 23:00:58 +1300

    added json-server and axios to graphql server. Now querying real data

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -7,12 +8,6 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
-
-const customers = [
-    {id:'1', name:'Jane Doe', email: 'jdoe@example.com', age:25},
-    {id:'2', name:'Sam Rankin', email: 'srankin@example.com', age:35},
-    {id:'3', name:'John Doe', email: 'john-doe@example.com', age:18}
-]
 
 const CustomerType = new GraphQLObjectType({
     name: 'Customer',
@@ -33,17 +28,15 @@ const RootQuery = new GraphQLObjectType({
                 id: {type: GraphQLString}
             },
             resolve(parentValue, args){
-                for(let i =0; i< customers.length; i++){
-                    if(customers[i].id === args.id){
-                        return customers[i];
-                    }
-                }
+                return axios.get('http://localhost:3000/customers/' + args.id)
+                    .then(res => res.data);
             }
         },
         customers:{
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args){
-                return customers;
+                return axios.get('http://localhost:3000/customers')
+                    .then(res => res.data);
             }
         }
     }
